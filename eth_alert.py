d7014ee9ec285fdd7386daf88cd115c9c7841bf0
mattromano <mattromano88@gmail.com>
2022-01-09 19:06:39 -0800

    added display matrix function

--- eth_alert.py
+++ eth_alert.py
@@ -1,32 +1,26 @@
-from typing import Text
+#from typing import Text
 from phue import Bridge
 import time
 import requests
-from sense_hat import SenseHat
+#from sense_hat import SenseHat
 
+def get_market_data():
+    response = requests.get('https://api.coingecko.com/api/v3/coins/ethereum?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
+    eth_price= response.json()
+    eth_market_data = eth_price['market_data']['price_change_percentage_1h_in_currency']['usd']
+    eth_market_data = ("%.2f" % eth_market_data)
+    return eth_market_data
 
-response = requests.get('https://api.coingecko.com/api/v3/coins/ethereum?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
-eth_price= response.json()
-#print(eth_price['price_change_percentage_1h_in_currency'])
-eth_market_data = eth_price['market_data']['price_change_percentage_1h_in_currency']['usd']
-print(type(eth_market_data))
+def matrix_display(eth_market_data):
+    sense = SenseHat()
+    red_message = sense.show_message(
+        "Market is fucked, ETH is down {} % in the past hour".format(get_market_data()), 
+        text_colour =[255,0,0])
+    green_message = sense.show_message(
+        "Market is mooning, ETH is up {} % in the hour".format(get_market_data()), 
+        text_colour =[0,255,0])
 
-red_message = sense.show_message("Market is fucked", text_colour =[255,0,0])
-green_message = sense.show_message("Market is mooning", text_colour =[0,255,0])
-
-#bridge_ip_address = '192.168.1.2'
-#b = Bridge(bridge_ip_address)
-
-
-#if eth_market_data > 0.0:
-    #green_lights()
-
-#elif eth_market_data < 0.0:
-   #red_lights()
-red_message()
-green_message()
-#print(eth_market_data)
-
-
-    
-
+    if eth_market_data() > 0.0:
+        return green_message
+    elif eth_market_data() < 0.0:
+        return red_message
